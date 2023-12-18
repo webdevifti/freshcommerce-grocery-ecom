d3e4d04aa82986a0812de9be1840e2ffb54894c1
Eftekhar Alam <eftialam@hotmail.com>
2023-12-18 19:49:34 +0600

    initial

---
+++ src/pages/Checkout.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import Footer from '../shared/Footer'
+import Header from '../shared/Header'
+
+const Checkout = () => {
+    return (
+        <div className="main__wrapper">
+            <Header />
+                <div>
+                    <h2>This is Checkout Page</h2>
+                </div>
+            <Footer />
+        </div>
+    )
+}
+
+export default Checkout
